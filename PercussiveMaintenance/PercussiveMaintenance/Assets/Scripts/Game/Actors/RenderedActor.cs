05d5c862669dde1e25b417f3cf315cd2120e35aa
Sean's Desk Alien <seanfowleruo@gmail.com>
2018-04-21 00:15:13 -0700

    Added Skeleton Code For Project Start

---
+++ PercussiveMaintenance/PercussiveMaintenance/Assets/Scripts/Game/Actors/RenderedActor.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderedActor : MonoBehaviour
+{
+    //This will be the base class for every actor in the scene.
+}
